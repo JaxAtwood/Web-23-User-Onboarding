7e7b9152a4c8b6e843bc9da5ec58e4409f4efca9
Jackie Atwood <takalaj@verizon.net>
2019-09-19 20:30:12 -0400

    Need to add YUP and touched and errors

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FaveMaynardSong from "./Component/Form";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Maynard Obsession Page</h1>
+      <div className="App-header">
+        <FaveMaynardSong />
+      </div>
+    </div>
+  );
+}
+
+export default App;
